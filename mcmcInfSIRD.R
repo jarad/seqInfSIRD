3940998b9acd8c1049e24dc6950c44a22f9d410b
Jarad Niemi <niemi@iastate.edu>
2011-11-07 21:32:52 -0600

    SIRD runs (poor mixing, wrong mode)

--- mcmcInfSIRD.R
+++ mcmcInfSIRD.R
@@ -9,12 +9,12 @@ fix.updates <- function(x,dx,p) {
     dx[2] <- rbinom(1, x[2]+dx[1], p)
     dx[4] <- x[2]+dx[1]-dx[2]
   }
-  dx[4] <- x[2]+dx[1]-dx[2] 
+  dx[3] <- min(c(dx[3],x[1]-dx[1])) 
   return(dx)
 }
 
 # Simulate data
-gamma <- rgamma(4,1); gamma <- c(.2,.1,.05,.01)
+gamma <- rgamma(4,1); gamma <- c(2,1,0,0)
 p <- rep(0.5,4)
 N <- 100
 I0 <- 2
@@ -26,11 +26,11 @@ dx[1,1] <- rpois(1, gamma[1]*X0[1]*X0[2]/N)
 dx[1,2] <- rpois(1, gamma[2]*X0[2])
 dx[1,3] <- rpois(1, gamma[3]*X0[1])
 dx[1,4] <- rpois(1, gamma[4]*X0[2])
-dx[1, ] <- fix.updates(X0,dx[1,],gamma[2]*X0[2]/(gamma[2]*X0[2]+gamma[4]*X0[2]))
+dx[1, ] <- fix.updates(X0,dx[1,],gamma[2]/(gamma[2]+gamma[4]))
 
 x[1,1] <- X0[1]-dx[1,1]        -dx[1,3]
 x[1,2] <- X0[2]+dx[1,1]-dx[1,2]        -dx[1,4]
-x[1,3] <- X0[3]                +dx[1,3]
+x[1,3] <- X0[3]        +dx[1,2]+dx[1,3]
 x[1,4] <- X0[4]                        +dx[1,4]
 
 for (j in 1:4) {
@@ -42,12 +42,11 @@ for (i in 2:n) {
   dx[i,2] <- rpois(1, gamma[2]*x[i-1,2])
   dx[i,3] <- rpois(1, gamma[3]*x[i-1,1])
   dx[i,4] <- rpois(1, gamma[4]*x[i-1,2])
-  dx[i,]  <- fix.updates(x[i-1,], dx[i,], 
-                         gamma[2]*x[i-1,2]/(gamma[2]*x[i-1,2]+gamma[4]*x[i-1,2]))
+  dx[i,]  <- fix.updates(x[i-1,], dx[i,], gamma[2]/(gamma[2]+gamma[4]))
 
   x[i,1] <- x[i-1,1]-dx[i,1]        -dx[i,3]
   x[i,2] <- x[i-1,2]+dx[i,1]-dx[i,2]        -dx[i,4]
-  x[i,3] <- x[i-1,3]                +dx[i,3]
+  x[i,3] <- x[i-1,3]        +dx[i,2]+dx[i,3]
   x[i,4] <- x[i-1,4]                        +dx[i,4]
 
   for (j in 1:4) {
@@ -56,9 +55,9 @@ for (i in 2:n) {
 }
 
 plot(x[,2])
-
+readline("Hit enter:")
 
 # Inference
-mod <- jags.model("SIRD.txt", list(X0=X0, y=y, p=p, n=nrow(y), N=sum(X0)),
-                  list(dx=y)) 
-res <- coda.samples(mod, c("gamma","x"), 1e3) 
+dat <- list(X0=X0, y=y, p=p, n=nrow(y), N=sum(X0), a=rep(1,4), b=rep(1,4))
+mod <- jags.model("SIRD.txt", dat, list(dx=dx),n.adapt=1e3) 
+res <- coda.samples(mod, c("gamma","x"), 1e3, thin=10) 
