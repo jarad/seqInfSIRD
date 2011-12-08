e3873cf1d6546c86e47b9c4b45a606b63827db96
Jarad Niemi <niemi@iastate.edu>
2011-12-08 17:40:25 -0500

    pgs working with example pgsNormalNormal.R

--- code/working/pgs.R
+++ code/working/pgs.R
@@ -2,14 +2,58 @@
 # 
 # Input:
 #   y  : data
-#   od : observation density (mass)
-#   er : evolution sampling
-#   gs : parameter sampler
+#   od : log observation density (mass)
+#   es : evolution sampler
+#   ps : parameter sampler
 #   J  : number of particles
-#   in.x : initial values
+#   pr : prior sampler for x
+#   iv : initial values
+#   n.iter : number of MCMC iterations
 #
-pgs <- function(y, od, er, gs, J, in.x) {
+# Output:
+#   theta : an n.iter x p matrix of fixed parameter samples
+#   x     : an n.iter x length(y) of latent states samples
+library(smcUtils)
+
+pgs <- function(y, od, er, ps, pr, iv, J=length(y)*5, n.iter=10) {
   n = length(y)
-  x = in$x
-  w = 
+  x = iv$x; theta = iv$theta # Initial values
+  w = rep(1,J)               # Particle weights
+  xp = A = matrix(NA,J,n)    # For scalar x
+  A[1,] = 1                  # Ancestor, current particle is included
+  
+  # Store samples
+  theta.reps = matrix(NA,n.iter,length(theta)) 
+  x.reps     = matrix(NA,n.iter,n)
+  
+  # MCMC
+  cat("  |",rep(" ",20),"|100%\n  |",sep="")
+  for (i in 1:n.iter) {
+  	if (i%%(n.iter/20)==0) cat("*")
+  	# Draw x	
+    xp[1,]   = x             # Current trajectory is included
+    xp[-1,1] = pr(J-1,theta) # Particle draws for x0
+    w        = renormalize.weights(od(y[1], xp[,1]), log=T)
+  	for (j in 2:n) {
+  	  A[-1,j]  = resample(w,J-1,"stratified")$indices
+  	  xp[-1,j] = er(xp[A[-1,j],j-1],theta)
+  	  w        = renormalize.weights(od(y[j],xp[A[,j],j]), log=T)  
+  	}
+  	
+  	# Sample path
+  	k = sample(J,1,prob=w)
+  	for (j in n:1) {
+  	  x[j] = xp[k,j]
+  	  k     = A[k,j]
+  	}
+  	
+  	
+  	theta = ps(x)
+  	
+  	theta.reps[i,] = theta
+  	x.reps[i,] = x
+  }
+  cat("|\n")
+  
+  return(list(theta=theta.reps, x=x.reps))
 }
