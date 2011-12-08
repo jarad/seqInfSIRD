8402e414699827ebf0f1a1ed00e49ef355c95a52
Jarad Niemi <niemi@iastate.edu>
2011-12-08 17:15:41 -0500

    pgs work in progress

---
+++ code/working/pgs.R
@@ -0,0 +1,15 @@
+# This function will run a naive univariate particle Gibbs sampler.
+# 
+# Input:
+#   y  : data
+#   od : observation density (mass)
+#   er : evolution sampling
+#   gs : parameter sampler
+#   J  : number of particles
+#   in.x : initial values
+#
+pgs <- function(y, od, er, gs, J, in.x) {
+  n = length(y)
+  x = in$x
+  w = 
+}
