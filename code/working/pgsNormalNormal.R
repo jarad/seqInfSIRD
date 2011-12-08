3764c04f3d53adb64deb88ca7fc1e679ac7fb8d5
Jarad Niemi <niemi@iastate.edu>
2011-12-08 17:52:42 -0500

    correction: pgsNormalNormal running, but not working

--- code/working/pgsNormalNormal.R
+++ code/working/pgsNormalNormal.R
@@ -10,13 +10,13 @@ y <- rnorm(n,x)
 source("pgs.R")
 
 out <- pgs(y, 
-           function(y,x,theta) dnorm(y,x,log=T), 
-           function(x,theta) rnorm(length(x), theta),
-           function(x) rnorm(1, mean(x), 1/sqrt(length(x))),
-           function(n,theta) rep(0,n),
+           function(y,x,theta) dnorm(y,x,log=T),             # y ~ N(x,1)
+           function(x,mu) rnorm(length(x), mu),        # x ~ N(mu,1)
+           function(x) rnorm(1, mean(x), 1/sqrt(length(x))), # mu|x 
+           function(n,mu) rnorm(n,mu),                 
            list(x=x*0, theta=mu), n.iter=1e3)
 
 
 hist(out$theta,100,freq=F)
-curve(dnorm(x, mean(y), 1/sqrt(n)), col="red", add=T)
+curve(dnorm(x, mean(y), sqrt(2/n)), col="red", add=T)
 
