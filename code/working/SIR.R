7090bf363c77c208cee83034ba44aaf4f2dcb28e
Jarad Niemi <niemi@iastate.edu>
2012-05-16 16:08:12 -0500

    added update function to SIR

--- code/working/SIR.R
+++ code/working/SIR.R
@@ -1,70 +1,48 @@
-if (!is.element("rjags",installed.packages()[,1])) install.packages("rjags")
-library(rjags)
+require(rjags)
 
+# Contains functions for SIR model
+#
+# SIRsim  : simulates SIR data
+# SIRmcmc : uses JAGS to run mcmc <not implemented yet>
 
-######### Functions ########
-fix.updates <- function(x,dx,p) {
-  dx[1] <- min(c(dx[1],x[1]))
-  if ( dx[2]+dx[4] > x[2]+dx[1]) {
-    dx[2] <- rbinom(1, x[2]+dx[1], p)
-    dx[4] <- x[2]+dx[1]-dx[2]
-  }
-  dx[3] <- min(c(dx[3],x[1]-dx[1])) 
+hazard = function(X,gamma,N=sum(X)) {
+  return(c(gamma[1]*X[1]*X[2]/N, # Hazard for S -> I
+           gamma[2]*X[2]))       # Hazard for I -> R
+}
+
+fix.updates <- function(dx,x) {
+  dx[1] = min(dx[1],x[1])       # max new infecteds is S
+  dx[2] = min(dx[2],x[2]+dx[1]) # max new recovereds is S+(S->I)
   return(dx)
 }
 
-# Simulate data
-gamma <- rgamma(4,1); gamma <- c(2,1,0,0)
-p <- rep(0.5,4)
-N <- 100
-I0 <- 2
-
-n <- 20
-x <- y <- dx <- matrix(NA,n,4)
-X0 <- c(N-I0,I0,0,0)
-dx[1,1] <- rpois(1, gamma[1]*X0[1]*X0[2]/N)
-dx[1,2] <- rpois(1, gamma[2]*X0[2])
-dx[1,3] <- rpois(1, gamma[3]*X0[1])
-dx[1,4] <- rpois(1, gamma[4]*X0[2])
-dx[1, ] <- fix.updates(X0,dx[1,],gamma[2]/(gamma[2]+gamma[4]))
-
-x[1,1] <- X0[1]-dx[1,1]        -dx[1,3]
-x[1,2] <- X0[2]+dx[1,1]-dx[1,2]        -dx[1,4]
-x[1,3] <- X0[3]        +dx[1,2]+dx[1,3]
-x[1,4] <- X0[4]                        +dx[1,4]
-
-for (j in 1:4) {
-  y[1,j] <- rbinom(1,dx[1,j],p[j])
+update = function(x,dx) {
+  c(x[1]-dx[1],x[2]+dx[1]-dx[2])
 }
 
-for (i in 2:n) {
-  dx[i,1] <- rpois(1, gamma[1]*x[i-1,1]*x[i-1,2]/N)
-  dx[i,2] <- rpois(1, gamma[2]*x[i-1,2])
-  dx[i,3] <- rpois(1, gamma[3]*x[i-1,1])
-  dx[i,4] <- rpois(1, gamma[4]*x[i-1,2])
-  dx[i,]  <- fix.updates(x[i-1,], dx[i,], gamma[2]/(gamma[2]+gamma[4]))
-
-  x[i,1] <- x[i-1,1]-dx[i,1]        -dx[i,3]
-  x[i,2] <- x[i-1,2]+dx[i,1]-dx[i,2]        -dx[i,4]
-  x[i,3] <- x[i-1,3]        +dx[i,2]+dx[i,3]
-  x[i,4] <- x[i-1,4]                        +dx[i,4]
-
-  for (j in 1:4) {
-    y[i,j] <- rbinom(1,dx[i,j],p[j])
+SIRsim = function(X0,gamma,p,n) {
+  x = y = dx = matrix(NA,n,2)
+  
+  # First step
+  dx[1,] = rpois(2,hazard(X0,gamma))
+  dx[1,] = fix.updates(dx[1,],X0)
+   x[1,] = update(X0,dx[1,])
+   y[1,] = rbinom(2,dx[1,],p)
+  
+  # Remaining steps
+  for (i in 2:n) {
+  	stopifnot(all(!is.na(x[i-1,])))
+  	dx[i,] = rpois(2,hazard(x[i-1,],gamma))
+  	dx[i,] = fix.updates(dx[i,],x[i-1,])
+  	 x[i,] = update(x[i-1,],dx[i,])
+  	 y[i,] = rbinom(2,dx[i,],p)
   }
+
+  return(list( x = data.frame(S    =  x[,1], I    =  x[,2]),
+              dx = data.frame(StoI = dx[,1], ItoR = dx[,2]),
+               y = data.frame(StoI =  y[,1], ItoR =  y[,2])))
 }
 
-plot(x[,2])
-readline("Hit enter:")
 
-# Inference
-dat <- list(X0=X0, y=y, p=p, n=nrow(y), N=sum(X0), a=rep(1,4), b=rep(1,4))
-mod <- jags.model("SIRD.txt", dat, list(dx=dx),n.adapt=1e3) 
-res <- coda.samples(mod, c("gamma","x"), 1e3, thin=10) 
 
-rownames(res$mcmc)
-par(mfrow=c(2,2))
-for (i in 1:4) {
-  plot(res[,i],trace=F,auto.layout=F)
-  abline(v=gamma[i],col="red",lwd=2)
-}
+
