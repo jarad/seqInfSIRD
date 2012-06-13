7ae4ec98a4dc844ad43b0c6e3dc0071862374f9a
Jarad Niemi <niemi@iastate.edu>
2012-06-13 10:29:17 -0500

    initial working script to perform mcmc on SIRDsims

---
+++ data/SIRDsims/SIRDsims-mcmc-script.R
@@ -0,0 +1,19 @@
+load("SIRDsims.RData")
+
+i = 1
+cols = 1:4
+dat   = list(X0=X0[cols], y=as.matrix(sims[[i]]$y[,cols]), p=probs[i,cols], 
+             n=nrow(sims[[i]]$y), 
+             N=sum(X0[cols]), a=prior$gamma$a[cols], b=prior$gamma$b[cols])
+inits = list(dx=as.matrix(sims[[i]]$dx[,cols]),
+             gamma=gammas[i,cols])
+
+mod   = jags.model("../../code/working/SIR.txt", data=dat, 
+                  inits=inits, n.adapt=1e3)
+res   = coda.samples(mod, c("gamma","x"), 1e3, thin=10) 
+
+
+
+mod   = jags.model("../../code/working/SIRD.txt", data=dat, 
+                  inits=inits, n.adapt=1e3)
+res   = coda.samples(mod, c("gamma","x"), 1e3, thin=10) 
