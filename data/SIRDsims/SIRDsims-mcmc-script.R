4bfcbd73a69068b4300bdcba8a0937b3cdc7f595
Jarad Niemi <niemi@iastate.edu>
2012-06-18 16:29:17 -0500

    mcmc now uses only SIR model

--- data/SIRDsims/SIRDsims-mcmc-script.R
+++ data/SIRDsims/SIRDsims-mcmc-script.R
@@ -1,19 +1,24 @@
 require(rjags)
 load("SIRDsims.RData")
 
+N.RXNS = 2
+N.STATES = 3
+
 i = 1
-cols = 1:4
-dat   = list(X0=X0[cols], y=as.matrix(sims[[i]]$y[,cols]), p=probs[i,cols], 
-             n=nrow(sims[[i]]$y), 
-             N=sum(X0[cols]), a=prior$theta$a[cols], b=prior$theta$b[cols])
-inits = list(dx=as.matrix(sims[[i]]$dx[,cols]),
-             theta=thetas[i,cols])
+  X0 = as.numeric(sims[[i]]$x[1,1:N.STATES])
+  dat   = list(X0 = X0, 
+               y  = as.matrix(sims[[i]]$y[1:n[i],1:N.RXNS]), 
+               p=probs[i,1:N.RXNS], 
+               n=n[i], 
+               N=sum(X0), a=prior$theta$a[1:N.RXNS], b=prior$theta$b[1:N.RXNS])
+  inits = list(dx=as.matrix(sims[[i]]$dx[1:n[i],1:N.RXNS]),
+               theta=thetas[i,1:N.RXNS])
 
 ptr = proc.time()
 
-mod   = jags.model("../../code/working/SIRD.txt", data=dat, 
-                  inits=inits, n.adapt=1e6)
-res   = coda.samples(mod, c("theta","x"), 1e6, thin=100) 
+mod   = jags.model("../../code/working/SIR.txt", data=dat, 
+                  inits=inits, n.adapt=1e2)
+res   = coda.samples(mod, c("theta","x"), 1e2, thin=10) 
 
 run.time = proc.time()-ptr
 
