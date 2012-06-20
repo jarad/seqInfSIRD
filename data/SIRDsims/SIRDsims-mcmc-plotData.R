e01dc38a1a83f6417ef44339fc7d924e399a20d6
Jarad Niemi <niemi@iastate.edu>
2012-06-20 16:24:04 -0500

    creating plot data

---
+++ data/SIRDsims/SIRDsims-mcmc-plotData.R
@@ -0,0 +1,34 @@
+load("SIRDsims-mcmc-script.RData")
+
+# Generate kernel density estimates for 
+# last time point of S, I, S->I, and I->R
+n.sims = length(res)
+
+kd = list()
+key = paste("simulation ",rep(1:n.sims,each=4),", ",c("S","I","S->I","I->R"),sep="")
+ii = 1
+for (i in 1:n.sims) {
+  kd[[ii]] = density(unlist(res[[i]][,2+n[i]])) # S
+  ii = ii+1
+  kd[[ii]] = density(unlist(res[[i]][,2+2*n[i]])) # I
+  ii = ii+1
+  kd[[ii]] = density(unlist(res[[i]][,1])) # S->I
+  ii = ii+1
+  kd[[ii]] = density(unlist(res[[i]][,2])) # I->R
+  ii = ii+1
+}
+
+save(kd,key,file="SIRDsims-mcmc-density.RData")
+
+# Save median and 95% credible intervals
+
+sims.MCMC = matrix(NA,n.sims,3*4)
+for (i in 1:n.sims) {
+  cols = c(2+n[i],1,2+2*n[i],2)
+  tmp = apply(as.matrix(res[[i]][,cols]), 2, function(x) quantile(x,prob=c(.5,.025,.975)))
+  sims.MCMC[i,] = as.vector(tmp[])
+}
+
+colnames(sims.MCMC) = paste(rep(c("S","SI","I","IR"),each=3),c("50","2.5","97.5"))
+write.csv(sims.MCMC,"SIRDsims-mcmc-quantiles.csv",row.names=F)
+this = read.csv("SIRDsims-mcmc-quantiles.csv")
