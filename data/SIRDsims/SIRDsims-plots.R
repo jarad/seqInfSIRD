733590e98b655cb8432d58de6003192b87e18c97
Jarad Niemi <niemi@iastate.edu>
2012-07-09 10:45:00 -0500

    added truth

--- data/SIRDsims/SIRDsims-plots.R
+++ data/SIRDsims/SIRDsims-plots.R
@@ -1,9 +1,16 @@
 # SIRD simulation plots
 
 load("SIRDsims-mcmc-density.RData")
+load("SIRDsims.RData")
+
+truth = NULL
+for (i in 1:length(n)) {
+  truth = c(truth,sims[[i]]$x$S[n[i]], sims[[i]]$x$I[n[i]], thetas[i,1:2])
+}
 
 par(mfrow=c(2,2))
 for (i in 1:length(kd)) {
   plot(kd[[i]], main=key[i])
+  abline(v=truth[i], col="red")
 }
 
