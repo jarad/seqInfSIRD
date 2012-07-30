033825ec999c55180683aeedcc0f8da4f8d344c4
Jarad Niemi <niemi@iastate.edu>
2012-07-30 11:44:09 -0500

    outbreak end can only occur after outbreak peak

--- data/SIRDsims/SIRDsims.R
+++ data/SIRDsims/SIRDsims.R
@@ -1,9 +1,10 @@
 source("../../code/working/SIRD.R")
 
+
 set.seed(1)
       
 # Number of steps              
-n = 52 # weekly for a year
+N = 100 # weekly for a year
 
 # Initial state
 X0 = c(16000,2,0,0)
@@ -27,7 +28,7 @@ probs  = cbind(parms$StoIprob, parms$ItoRprob, 0, 0)
 
 ##################### Generate simulations ########################
 sims = list()
-for (i in 1:nrow(thetas)) sims[[i]] = SIRDsim(X0,thetas[i,],probs[i,],n)
+for (i in 1:nrow(thetas)) sims[[i]] = SIRDsim(X0,thetas[i,],probs[i,],N)
 
 prior = list()
 prior$theta = list()
@@ -38,12 +39,16 @@ prior$p$b     = c(1,1,1,1)
 
 # Find the end of the outbreak
 # defined as 4 consecutive weeks of zero S->I observed
+n = numeric(length(sims))
 for (i in 1:nrow(thetas)) {
   zeros = which(sims[[i]]$y$StoI==0)
+  ymax  = which.max(sims[[i]]$y$StoI)
+  zeros = zeros[zeros>ymax]
   diff3 = diff(zeros,3)
-  n[i] = min(zeros[match(3,diff3)]+3,52)
+  n[i] = min(zeros[match(3,diff3)]+3,N)
 }
 
 save(sims,thetas,probs,n,prior, file="SIRDsims.RData")
 
-#q("no")
+q("no")
+
