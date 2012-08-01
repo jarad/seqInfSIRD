e62626a044e4ba60a6822e0143e93fafa3ea4fa8
Jarad Niemi <niemi@iastate.edu>
2012-08-01 14:47:10 -0500

    hazard function implemented in R

--- code/working/inference.r
+++ code/working/inference.r
@@ -1,3 +1,14 @@
 # Functions for discrete-time compartment models
 
+hazard.part = function(sys, engine="R") {
+    if (engine!="R") stop("Only R is implemented")
+    stopifnot(all(sys$Pre%in%c(0,1))) # Dimerizations or above not supported 
+  
+    hp = numeric(sys$r)
+    for (i in 1:sys$r) hp[i] = prod(sys$X^sys$Pre[i,])
+    return(hp)
+}
 
+hazard = function(sys, theta,...) {
+    return(theta*hazard.part(sys,...))
+}
