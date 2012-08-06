1aa3e6f5c67d41df24edee42db2c2513388dfaa8
Jarad Niemi <niemi@iastate.edu>
2012-08-06 12:25:59 -0500

    more error checking for sim.R

--- code/tlpl/R/sim.R
+++ code/tlpl/R/sim.R
@@ -1,8 +1,25 @@
 if (!is.loaded("sim")) dyn.load("../src/tlpl.so")
 
+is.wholenumber = function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
+
+
 sim = function(sys, n, tau, while.max=1000) 
 {
+    # Error checking
     check.system(sys)
+    stopifnot(tau>0, n>0, while.max>0)
+
+    if (!is.wholenumber(n)) 
+    {
+        warning("Since n is not a whole number, using round(n) instead.")
+        n = round(n)
+    }
+    if (!is.wholenumber(while.max)) 
+    {
+        warning("Since while.max is not an integer, using round(while.max) instead.")
+        while.max=round(while.max)
+    }
+
 
     out = .C("sim",
              as.integer(sys$s), as.integer(sys$r), as.integer(sys$stoich), as.integer(t(sys$Pre)), 
