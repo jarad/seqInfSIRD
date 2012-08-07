d8903559825e8fa017a22ce0ca8fec37079ae43c
Jarad Niemi <niemi@iastate.edu>
2012-08-07 15:31:41 -0500

    removed dangling }

--- code/tlpl/src/resample.c
+++ code/tlpl/src/resample.c
@@ -1,12 +1,12 @@
 #include <R.h>
 #include <Rmath.h>
 
-/* Renormalizes the vector adWeights to sum to 1. If *log \ne 0, the adWeights are assumed to be log adWeights */
-void renormalize(const int *n, const int *log, double *adWeights) 
+/* Renormalizes the vector adWeights to sum to 1. If *nLog \ne 0, the adWeights are assumed to be log adWeights */
+void renormalize(const int *n, const int *nLog, double *adWeights) 
 {
   int i;
 
-  if (*log) {
+  if (*nLog) {
     double max=adWeights[0];
     for (i=1; i<*n; i++) if (adWeights[i]>max) max = adWeights[i]; 
     for (i=0; i<*n; i++) adWeights[i] = exp(adWeights[i]-max);
@@ -15,11 +15,10 @@ void renormalize(const int *n, const int *log, double *adWeights)
   double sum=0;
   for (i=0; i<*n; i++) sum += adWeights[i];
   for (i=0; i<*n; i++) adWeights[i] /= sum;
-  }
 }
 
 
-/* Performs multinomial resampling on the adWeights adWeights. Returns the anIndices for nSamples samples */
+/* Performs multinomial resampling on the adWeights. Returns the anIndices for nSamples samples */
 void multinomial_resample(const int *n, const int *nSamples, const double *adWeights, int *anIndices) 
 {
   int i, j;
