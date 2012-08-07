688ce19b0b10457f7aed50ab7cb18c261fc57d65
Jarad Niemi <niemi@iastate.edu>
2012-08-07 15:25:36 -0500

    added multinomial resampling function

--- code/tlpl/src/resample.c
+++ code/tlpl/src/resample.c
@@ -1,19 +1,40 @@
+#include <R.h>
 #include <Rmath.h>
 
-/* Renormalizes the vector w to sum to 1. If *log \ne 0, the weights are assumed to be log weights */
-void renormalize(const int *n, const int *log, double *w) {
+/* Renormalizes the vector adWeights to sum to 1. If *log \ne 0, the adWeights are assumed to be log adWeights */
+void renormalize(const int *n, const int *log, double *adWeights) 
+{
   int i;
 
   if (*log) {
-    double max=w[0];
-    for (i=1; i<*n; i++) if (w[i]>max) max = w[i]; 
-    for (i=0; i<*n; i++) w[i] = exp(w[i]-max);
+    double max=adWeights[0];
+    for (i=1; i<*n; i++) if (adWeights[i]>max) max = adWeights[i]; 
+    for (i=0; i<*n; i++) adWeights[i] = exp(adWeights[i]-max);
   }
 
   double sum=0;
-  for (i=0; i<*n; i++) sum += w[i];
-  for (i=0; i<*n; i++) w[i] /= sum;
+  for (i=0; i<*n; i++) sum += adWeights[i];
+  for (i=0; i<*n; i++) adWeights[i] /= sum;
   }
 }
 
+
+/* Performs multinomial resampling on the adWeights adWeights. Returns the anIndices for nSamples samples */
+void multinomial_resample(const int *n, const int *nSamples, const double *adWeights, int *anIndices) 
+{
+  int i, j;
+  double cusum[*n];
+
+  /* Calculate the cumulative sum of the adWeightseight series */
+  cusum[0] = adWeights[0];
+  for (i=1; i<*n; i++) cusum[i] = cusum[i-1]+adWeights[i];
+  
+  GetRNGstate();
+  for (i=0; i<*nSamples; i++) {
+    j=0;
+    while (cusum[j]< runif(0,1)) { j++; }
+    anIndices[i] = j;
+  }
+  PutRNGstate();
+}
 
