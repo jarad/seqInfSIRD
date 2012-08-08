5c39cb719a37f0a11053dcb58f5389f7ef14e345
Jarad Niemi <niemi@iastate.edu>
2012-08-07 21:50:28 -0500

    residual_resample added, but multinomial_resample is not working

--- code/tlpl/src/resample.c
+++ code/tlpl/src/resample.c
@@ -43,9 +43,26 @@ void cumulative_sum(const int n, double *v)
     for (i=1; i<n; i++) v[i] += v[i-1];
 }
 
+void rep2id(const int *n, int *rep, int *sum, int *id)
+{
+    int i, j=0;
 
+    i=0;
+    while (i<*sum) 
+    {
+        if (rep[j]>0) // If this particle is resampled (again)
+        {
+            rep[j]--; 
+            id[i] = j;
+            i++;
+        } 
+        else          // If all resamples of this particle are exhausted
+        {
+            j++;
+        }
+    }
+}
 
-/************************ Resampling functions *****************************************/
 
 void inverse_cdf_weights(const int *nWeights, 
                          double *adWeights, 
@@ -81,21 +98,29 @@ void inverse_cdf_weights(const int *nWeights,
 
 
 
+/************************ Resampling functions *****************************************/
+
+
+
+
+
+
+
 
 /* Renormalizes the vector adWeights to sum to 1. If *nLog \ne 0, the weights are assumed to be log weights */
-void renormalize(const int *nWeights, const int *nLog, double *adWeights) 
+void renormalize(int nWeights, int nLog, double *adWeights) 
 {
     int i;
 
-    if (*nLog) {
+    if (nLog) {
         double max=adWeights[0];
-        for (i=1; i<*nWeights; i++) if (adWeights[i]>max) max = adWeights[i]; 
-        for (i=0; i<*nWeights; i++) adWeights[i] = exp(adWeights[i]-max);
+        for (i=1; i<nWeights; i++) if (adWeights[i]>max) max = adWeights[i]; 
+        for (i=0; i<nWeights; i++) adWeights[i] = exp(adWeights[i]-max);
     }
 
     double sum=0;
-    for (i=0; i<*nWeights; i++) sum += adWeights[i];
-    for (i=0; i<*nWeights; i++) adWeights[i] /= sum;
+    for (i=0; i<nWeights; i++) sum += adWeights[i];
+    for (i=0; i<nWeights; i++) adWeights[i] /= sum;
 }
 
 
@@ -132,15 +157,54 @@ void stratified_resample(const int *nWeights, double *adWeights, const int *nInd
 
 void systematic_resample(const int *nWeights, double *adWeights, const int *nIndices, int *anIndices)
 {
-    double adUniforms[*nIndices];
-    adUniforms[0] = runif(0, (float) 1/ *nIndices);
     int i;
+    double adUniforms[*nIndices];
+    GetRNGstate();
+    adUniforms[0] = runif(0, (float) 1/ *nIndices);
+    PutRNGstate();
     for (i=1; i<*nIndices; i++) adUniforms[i] =  adUniforms[i-1]+ (float) 1 / *nIndices;
 
     inverse_cdf_weights(nWeights, adWeights, nIndices, adUniforms, anIndices);
 }
 
+void residual_resample(const int *nWeights, double *adWeights, int *nIndices, int *anIndices,
+                       const int *nResidualResampleFunction)
+{
+    int i, anDeterministicReps[*nWeights], nDeterministicReps=0;
+    double adExpectedSamples[*nWeights];
+    for (i=0; i<*nWeights; i++) 
+    {
+        adExpectedSamples[i]    = adWeights[i]* *nIndices;
+        anDeterministicReps[i]  = adExpectedSamples[i];
+        adWeights[i]            = adExpectedSamples[i]-anDeterministicReps[i];
+        nDeterministicReps     += anDeterministicReps[i];
+    }
+    if (nDeterministicReps > *nIndices) 
+        REprintf("C: residual_resample: too many deterministic reps\n");
+   
+    rep2id(nWeights, anDeterministicReps, &nDeterministicReps, anIndices);
+    *nIndices -= nDeterministicReps;
 
+    Rprintf("Remaining samples: %d\n", *nIndices);
+    Rprintf("Deterministic reps: %d\n", nDeterministicReps);
+
+    renormalize(*nWeights, 0, adWeights);
+    switch (*nResidualResampleFunction) 
+    {
+        case 1:
+            stratified_resample( nWeights, adWeights, nIndices, &anIndices[nDeterministicReps]);
+            break;
+        case 2:
+            multinomial_resample(nWeights, adWeights, nIndices, &anIndices[nDeterministicReps]);
+            break;
+        case 3:
+            systematic_resample( nWeights, adWeights, nIndices, &anIndices[nDeterministicReps]);
+            break;
+        default:
+            REprintf("C: residual_resample: no match for residual resampling function\n");
+    }
+       
+}
 
 
 
