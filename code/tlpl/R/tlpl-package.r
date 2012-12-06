68a008529dd274e6b0b111ed0d1268adf8376185
Jarad Niemi <niemi@iastate.edu>
2012-12-06 10:35:10 -0800

    added tlpl-package.r

---
+++ code/tlpl/R/tlpl-package.r
@@ -0,0 +1,14 @@
+#' Implements tau-leaped particle learning
+#'
+#' \tabular{ll}{
+#' Package: \tab tlpl\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1\cr
+#' Date: \tab 2012-12-06\cr
+#' License: \tab GPL-3\cr
+#' LazyLoad: \tab yes\cr
+#' Depends: stats\cr
+#' Suggests: smcUtils, testthat
+#' }
+#' 
+
