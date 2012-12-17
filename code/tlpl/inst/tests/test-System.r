2f31299615b71dcdc8101fce9a98d94f25c738ae
Jarad Niemi <niemi@iastate.edu>
2012-12-17 12:33:22 -0600

    moved tlpl directory to its own repository

--- code/tlpl/inst/tests/test-System.r
+++
@@ -1,8 +0,0 @@
-context("System.r")
-
-test_that("test.system throws error",
-{
-    expect_error(test.system(3),"Test system doesn't exist.")
-    expect_error(test.system(4),"Test system doesn't exist.")
-})
-
