d4301a2282079573b3b2cc7413dda0bb0dbfde13
Jarad Niemi <niemi@iastate.edu>
2012-12-20 11:27:15 -0600

    added initial liu_west algorithm...need to debug

--- data/SIR/LW.R
+++ data/SIR/LW.R
@@ -1,6 +1,15 @@
 # Performs LW for preset tuning parameter
 
+load("sims.RData")
+source("filter-settings.R")
+source("liu_west.r")
+
 lw = list()
+
+for (i in 1:n.sims)
+{
+  lw[[i]] = liu_west(data[[i]], sckm, n.particles=n.particles, delta=delta, prior=prior)
+}
 
 save(lw, file=paste("LW", round(delta*100), ".RData", sep=""))
 
---
+++ data/SIR/liu_west.r
@@ -0,0 +1,79 @@
+library(tlpl)
+library(smcUtils)
+
+logit = function(p) log(p/(1-p))
+expit = function(r) 1/(1+1/exp(r))
+
+liu_west = function(y, sckm, n.particles, delta, prior)
+{
+  # Liu-West parameter
+  a = (3*delta-1)/(2*delta)
+  h = sqrt(1-a^2)
+
+  # Indices
+  p.i = 1:sckm$r
+  r.i = sckm$r + (1:sckm$r)
+
+  # Set up data structures
+  n = nrow(y)
+  weights = matrix(1/n.particles, n.particles, n+1)
+  lweights = log(weights)
+  X = array(NA, dim=c(sckm$s, n.particles, n+1))
+  theta = array(NA, dim=c(2*sckm$r, n.particles, n+1))
+
+  ## Initialize
+  X[,,1] = sckm$X
+  for (i in 1:sckm$r) 
+  {
+    theta[i,,1]        = logit(rbeta( n.particles, prior$prob$a[i], prior$prob$b[i]))
+    theta[i+sckm$r,,1] = log(  rgamma(n.particles, prior$rate$a[i], prior$rate$b[i]))
+  }
+ 
+  # Variables used in algorithm
+  w = numeric(n.particles)
+  m = matrix(NA, 2*sckm$r, n.particles)
+
+  for (i in 1:n)
+  {
+    # Perform fixed parameter jittering
+    mn = apply(theta[,,i], 1, mean)
+    cv = cov(t(theta[,,i]))
+    ch = t(chol(cv, pivot=TRUE))
+    for (j in 1:n.particles)
+    {
+      m[,j] = a*theta[,j,i] + (1-a)*mn
+    }
+
+    # Calculate APF weights
+    for (j in 1:n.particles) 
+    {
+      sckm$X = X[,j,i]
+      sckm$theta = exp(theta[r.i,j,i])
+      exp.nr = ceiling(hazard(sckm)$h) # Expected number of reactions/transitions
+      w[j] = lweights[j,i] + sum(dbinom(y[i,], exp.nr, expit(m[p.i,j]), log=T))
+    }
+
+    # Resample particles
+    w = renormalize.weights(w, log=T)
+    kk = resample(w, method=resampling.function)
+
+    # Propagate
+    for (j in 1:n.particles)
+    {
+      sckm$theta = theta[,j,i+1] = m[,kk[j]] + h*ch%*%rnorm(2*sckm$r)
+      sckm$X = X[,kk[j],i]
+      tmp = tau_leap(sckm)
+      X[,j,i+1] = tmp$X[2,]
+      lweights[j,i+1] = sum(dbinom(y[i,], tmp$nr, expit(theta[p.i,j,i+1]), log=T)-
+                            dbinom(y[i,], exp.nr, expit(    m[p.i,j,i  ]), log=T))
+
+    }
+    weights[,i+1] = renormalize.weights(lweights[,i+1], log=T)
+  }
+
+  return(list(weights=weights, X=X, 
+              p = expit(theta[1:sckm$r,,]), 
+              r = exp(theta[sckm$r + (1:sckm$r),,])))
+}
+
+
