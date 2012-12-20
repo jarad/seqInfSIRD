f9070710f08c16ac2020152904010a48779eb105
Jarad Niemi <niemi@iastate.edu>
2012-12-20 14:56:21 -0600

    LW quantiles now uses Hmisc::wtd.quantile, but does not deal with all points being NA

--- data/SIR/LWq.R
+++ data/SIR/LWq.R
@@ -6,14 +6,29 @@ source("quantile-settings.R")
 
 lwq = list()
 
+n = dim(lw[[1]]$X)[3]
+p = length(probs)
+s = dim(lw[[1]]$X)[1]
+r = dim(lw[[1]]$p)[1]
+
+Xq = array(NA, dim=c(s,p,n))
+pq = rq = array(NA, dim=c(r,p,n))
+
 for (i in 1:length(lw))
 {
-  lwq[[i]] = list(X.quantiles = apply(lw[[i]]$X, c(1,3), quantile, probs=probs, na.rm=T), 
-                  p.quantiles = apply(lw[[i]]$p, c(1,3), quantile, probs=probs, na.rm=T), 
-                  r.quantiles = apply(lw[[i]]$r, c(1,3), quantile, probs=probs, na.rm=T))
+  for (j in 1:n)
+  {
+    Xq[,,j] = apply(lw[[i]]$X[,,i], 1, wtd.quantile, probs=probs, weights=lw[[i]]$weights[,j])
+    pq[,,j] = apply(lw[[i]]$p[,,i], 1, wtd.quantile, probs=probs, weights=lw[[i]]$weights[,j])
+    rq[,,j] = apply(lw[[i]]$r[,,i], 1, wtd.quantile, probs=probs, weights=lw[[i]]$weights[,j])
+    
+    lwq[[i]] = list(X.quantiles = Xq, 
+                    p.quantiles = pq, 
+                    r.quantiles = rq)
+  }
 }
 
 save(lwq, file=paste("LWq", round(delta*100), ".RData", sep=""))
 
-q("no")
+#q("no")
 
