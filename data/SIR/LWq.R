ad8c133bd7c37955c3540fd4e5ab23d528cac691
Jarad Niemi <niemi@iastate.edu>
2012-12-21 10:08:43 -0600

    now uses my.wtd.quantile to take care of NAs

--- data/SIR/LWq.R
+++ data/SIR/LWq.R
@@ -14,18 +14,26 @@ r = dim(lw[[1]]$p)[1]
 Xq = array(NA, dim=c(s,p,n))
 pq = rq = array(NA, dim=c(r,p,n))
 
+
+my.wtd.quantile = function(x,...)
+{
+  if (all(is.na(x)) | length(x)==0) return(NA)
+
+  wtd.quantile(x,...)
+}
+
 for (i in 1:length(lw))
 {
   for (j in 1:n)
   {
-    Xq[,,j] = apply(lw[[i]]$X[,,i], 1, wtd.quantile, probs=probs, weights=lw[[i]]$weights[,j])
-    pq[,,j] = apply(lw[[i]]$p[,,i], 1, wtd.quantile, probs=probs, weights=lw[[i]]$weights[,j])
-    rq[,,j] = apply(lw[[i]]$r[,,i], 1, wtd.quantile, probs=probs, weights=lw[[i]]$weights[,j])
-    
-    lwq[[i]] = list(X.quantiles = Xq, 
-                    p.quantiles = pq, 
-                    r.quantiles = rq)
-  }
+    Xq[,,j] = apply(lw[[i]]$X[,,j], 1, my.wtd.quantile, probs=probs, weights=lw[[i]]$weights[,j])
+    pq[,,j] = apply(lw[[i]]$p[,,j], 1, my.wtd.quantile, probs=probs, weights=lw[[i]]$weights[,j])
+    rq[,,j] = apply(lw[[i]]$r[,,j], 1, my.wtd.quantile, probs=probs, weights=lw[[i]]$weights[,j])
+  }    
+
+  lwq[[i]] = list(X.quantiles = Xq, 
+                  p.quantiles = pq, 
+                  r.quantiles = rq)
 }
 
 save(lwq, file=paste("LWq", round(delta*100), ".RData", sep=""))
