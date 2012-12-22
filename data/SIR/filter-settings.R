e83a3f6af74a9079a53b5720cd2ba00df51c0db9
Jarad Niemi <niemi@iastate.edu>
2012-12-22 09:48:35 -0600

    laptop settings

--- data/SIR/filter-settings.R
+++ data/SIR/filter-settings.R
@@ -1,3 +1,3 @@
-n.particles = 1000
+n.particles = 100
 resampling.function = "multinomial"
 
--- data/SIR/sims.R
+++ data/SIR/sims.R
@@ -14,7 +14,7 @@ N = sum(sckm$X)
 sckm$lmult = log(c(1/N,1))
 
 ## Simulate data
-n.sims = 50
+n.sims = 10
 set.seed(20121218)
 n = 50
 
