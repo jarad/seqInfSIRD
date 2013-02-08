db0820aa84642a57d88a42a32b66e2660ff6b29c
Jarad Niemi <niemi@iastate.edu>
2013-02-08 15:54:01 -0600

    combines data

--- data/SIR/summary-plots.R
+++ data/SIR/summary-plots.R
@@ -1,8 +1,17 @@
 
-d = read.csv("summary-statistics.csv")
+fl = system("ls *sum.csv",T)
+
+d = list()
+for (i in 1:length(fl)) d[[i]] = read.csv(fl[i])
+
+library(plyr)
+library(reshape2)
+d = dcast(rbind.fill(d), parameter + time + method ~ statistic)
+
+d$parameter = factor(d$parameter, c("S","I","R","p: S->I","p: I->R","blank","r: S->I","r: I->R"))
 
 library(ggplot2)
-qplot(time, value, data=d[d$stat=="MSE",], facets=~parameter)
+qplot(time, MCV, data=d, geom="line", colour=method, facets=~parameter)
 
 
 
